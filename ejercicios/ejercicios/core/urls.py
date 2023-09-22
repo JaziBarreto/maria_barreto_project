967f8314276a2c478208358e1ffcf8ae4fc91d24
Maria Jazmin Barreto Pereira <131320172+JaziBarreto@users.noreply.github.com>
2023-09-22 13:36:12 -0400

    Delete ejercicios directory

--- ejercicios/ejercicios/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path     
-from . import views
-
-#los url parten de lo que este en urls.py de mi modulo principal (ejercicios)
-urlpatterns = [
-    path('', views.agregar_usuario),
-    # path('formulario', views.some_function)
-
-]
