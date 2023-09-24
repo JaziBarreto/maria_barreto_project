90dcba9ad4a8b7d574bf7a813fc1ad6b26ce2afc
Maria Jazmin Barreto Pereira <131320172+JaziBarreto@users.noreply.github.com>
2023-09-24 11:23:56 -0400

    Add files via upload

---
+++ ejercicio/ejercicio/usuario_app/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.utils.translation import gettext_lazy as _
+
+class CustomUser(AbstractUser):
+    # Campos heredados de AbstractUser
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=20, unique=True)
+
+    # Campos personalizados
+    peso = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    foto_perfil = models.ImageField(upload_to='perfil/', null=True, blank=True)
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='customuser_set')
+    user_permissions = models.ManyToManyField(
+        Permission,
+        verbose_name=_('user permissions'),
+        blank=True,
+        related_name='customuser_set',
+        help_text=_('Specific permissions for this user.'),
+    )
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+    def __str__(self):
+        return self.username
