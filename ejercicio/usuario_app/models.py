33304943e56ed7d939d57a4968b1a634f67c1874
Maria Jazmin Barreto Pereira <131320172+JaziBarreto@users.noreply.github.com>
2023-09-24 09:39:22 -0400

    Add files via upload

---
+++ ejercicio/usuario_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    username = models.CharField(max_length=20, unique=True, verbose_name="Nombre de Usuario")
+    email = models.EmailField(max_length=200, unique=True)
+    password = models.CharField(max_length=72)
+    birthday = models.DateField()
+    peso = models.IntegerField()
+    foto_perfil = models.ImageField(upload_to='perfil/', null=True, blank=True)  # Campo para la foto de perfil
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
